a9429413a04aa85f370180eea09f06a8c963fa58
Ossan Msoili <ossanmsoili@yahoo.com>
2023-12-05 22:42:03 +0100

    il est dans l'ordre alphabétique mdr

--- sokoban.h
+++ sokoban.h
@@ -22,53 +22,44 @@ typedef struct {
     int playing;
     int stuck;
 }size_tab;
-typedef struct {
-    int x;
-    int y;
-}pos_goal;
-typedef struct {
-    int x;
-    int y;
-} Position;
-int my_strcmp(char const *s1, char const *s2);
-char my_putchar(char a);
-int my_putstr(char const *str);
-void my_display_in_center(WINDOW *stdscr, char **world, size_tab *s,
-    element *e);
-int num_len(int n);
-int my_put_nbr(int nb);
-void display(char **world, size_tab *s, element *e, char **backup_world);
-int my_strlen(char const *str);
-int sokoban(char **av, int fd, element *e);
-int is_help(char **av, element *e);
-void my_help(void);
-char **my_convert_tab(char const *word, char **world, int size, int longeur_j);
 void append(char **world, int i, int j, char const *word);
-int my_column(char const *word);
-int my_length(char const *word);
-int create_tab(char const *word, element *e);
-void search_player(char **world, element *e);
-void where_is_player(char **world, element *e, int i, int j);
-int verif_word(char const *word);
-int moving_player(element *e, char **world, char **back_world, int t_pressed);
-int is_moving(element *e, char **world, int move_x, int move_y);
-int is_moving_with_box_y(element *e, char **world, int move_y);
-int is_moving_with_box_x(element *e, char **world, int move_x);
-int is_moving(element *e, char **world, int move_x, int move_y);
-int search_goal(char **world, size_tab *s);
-int is_goal(char **world, size_tab *s, int i, int j);
-int my_box(char **world, size_tab *s, int search_goal);
+void append2(char **world, int i, int j, char const *word);
 int comparate_my_box(int blocked, int count_box_stuck, int search_goal,
     size_tab *s);
-int verify_box(char **world, int i, int j);
-void test(char w, int i, int j, element *e);
-char **reset_game(char **world, size_tab *s, char **backup_world,
-    element *e);
+void compare_world(char **back_world, char **world, size_tab *s);
+int create_tab(char const *word, element *e);
+void display(char **world, size_tab *s, element *e, char **backup_world);
+int end_game(char **world, size_tab *s);
+int is_goal(char **world, size_tab *s, int i, int j);
+int is_help(char **av, element *e);
+int is_moving(element *e, char **world, int move_x, int move_y);
+int is_moving_with_box_x(element *e, char **world, int move_x);
+int is_moving_with_box_y(element *e, char **world, int move_y);
+int moving_player(element *e, char **world, char **back_world, int t_pressed);
+int my_box(char **world, size_tab *s, int search_goal);
+int my_column(char const *word);
+char **my_convert_tab(char const *word, char **world, int size, int longeur_j);
 char **my_convert_tab_void(char const *word, char **world, int size,
     int longeur_j);
-void append2(char **world, int i, int j, char const *word);
-void compare_world(char **back_world, char **world, size_tab *s);
+void my_display_in_center(WINDOW *stdscr, char **world, size_tab *s,
+    element *e);
+void my_help(void);
+int my_length(char const *word);
+char my_putchar(char a);
+int my_put_nbr(int nb);
+int my_putstr(char const *str);
+int my_strcmp(char const *s1, char const *s2);
+int my_strlen(char const *str);
+int num_len(int n);
+void search_box(char **world, element *e);
+int search_goal(char **world, size_tab *s);
+void search_player(char **world, element *e);
+int sokoban(char **av, int fd, element *e);
+void test(char w, int i, int j, element *e);
 void verif_comparaison(char **back_world, char **world, int i, int j);
-int end_game(char **world, size_tab *s);
 int verif_end_game(char w);
+int verif_word(char const *word);
+int verify_box(char **world, int i, int j);
+void where_is_box(char **world, element *e, int i, int j);
+void where_is_player(char **world, element *e, int i, int j);
 #endif
